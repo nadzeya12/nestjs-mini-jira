87599b2aea02d0820fee5515cdf3837dae2775b5
nadzeya12 <nadezdapratashchyk@gmail.com>
2026-05-12 15:20:23 +0200

    add tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-// import { Body, Controller, Post } from '@nestjs/common';
-// import { CreateTaskDto } from './dto/create.task.dto';
-  
-// @Controller('tasks')
-// export class TasksController {
-//   constructor(private readonly tasksService: TasksService) {}
-
-//   @Post('projects')
-//     create(@Body() dto: CreateTaskDto) {
-//       return this.tasksService.createTask(dto);
-//     }
-// }
-=======
 import { Body, Controller, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { CreateTaskDto } from './dto/create.task.dto';
@@ -25,4 +11,3 @@ export class TasksController {
       return this.tasksService.createTask(dto);
     }
 }
->>>>>>> e2e2b1f4c97ff4adf546cd530fb91113adc5f91e
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,34 +1,3 @@
-<<<<<<< HEAD
-// import { Injectable } from '@nestjs/common';
-// import { InjectRepository } from '@nestjs/typeorm';
-// import { tasksEntity } from './entities/task.entity';
-// import { Repository } from 'typeorm';
-// import { CreateTaskDto } from './dto/create.task.dto';
-// import { ProjectsService } from '../projects/projects.service';
-
-// @Injectable()
-// export class TasksService {
-//     constructor(
-//         @InjectRepository (tasksEntity)
-//          private readonly  taskRepository: Repository<tasksEntity>,
-//         private readonly projectService: ProjectsService,
-//     ) {}
-
-//     async createTask (dto: CreateTaskDto): Promise<tasksEntity> {
-//         const {projectId, title, description, status} = dto;
-
-//         const project = await this.projectService.findById(projectId);
-
-//         const task = this.taskRepository.create({
-//             title, 
-//             description,
-//             status,
-//             project,    
-//         });     
-//         return await this.taskRepository.save(task);
-//     }
-// }
-=======
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { tasksEntity } from './entities/task.entity';
@@ -58,4 +27,3 @@ export class TasksService {
         return await this.taskRepository.save(task);
     }
 }
->>>>>>> e2e2b1f4c97ff4adf546cd530fb91113adc5f91e
