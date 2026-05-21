19b2fdf9677c9696254655d271ed36d404622d6b
NP <nadezdapratashchyk@gmail.com>
2026-05-21 21:04:12 +0200

    request.user-OK

---
+++ project.owner.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivate, ExecutionContext, ForbiddenException, Injectable, NotFoundException } from "@nestjs/common";
+import { ProjectsService } from "../projects.service";
+import { userEntity } from "../../users/entities/user.entity";
+
+//OK
+@Injectable()
+export class projectOwnerGuard implements CanActivate {
+    constructor (private readonly projectsService: ProjectsService) {}
+
+    async canActivate(context: ExecutionContext): Promise <boolean> {
+        const request = context.switchToHttp().getRequest();
+
+        const user: userEntity = request.headers.token;
+
+        const projectId: string = request.Param.id;
+
+        if(!user) {
+            throw new ForbiddenException('No loginned user');
+        }
+
+        const project = await this.projectsService.findById(projectId);
+
+        if(!project) {
+            throw new NotFoundException('Project not found.')
+        }
+
+        if(String(project.userId) !== String(user.id)) {
+            throw new ForbiddenException('No access.')
+        }
+
+        request.project = project;
+        return true;
+    }
+}
---
+++ projects.service.ts
@@ -0,0 +1,67 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { projectEntity } from './entities/project.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateProjectDto } from './dto/create.project.dto';
+import { randomBytes } from 'crypto';
+import { userEntity } from '../users/entities/user.entity';
+
+//OK
+@Injectable()
+export class ProjectsService {
+  constructor (
+    @InjectRepository(projectEntity) 
+    private readonly projectRepository: Repository<projectEntity>,
+    @InjectRepository(userEntity)
+    private readonly usersRepository: Repository<userEntity>
+  ) {}
+  
+  async findAllByUser(userId: string): Promise<projectEntity[]> {
+
+    const existsUser = await this.usersRepository.exists({
+      where: { id: userId}
+    });
+
+    if (!existsUser) {
+      throw new NotFoundException('User does not exist')
+    }
+
+    return await this.projectRepository.find({ 
+      where: {userId: userId}
+    });
+  }
+
+  async findById(id: string): Promise<projectEntity> {
+    const project = await this.projectRepository.findOne({
+      where: {
+        id: id
+      },
+    });
+
+    if(!project) throw new NotFoundException('Project not found')
+
+    return project;
+  }
+
+  async deleteProject(id: string) {
+    const project = await this.projectRepository.findOneBy({id});
+
+    if(!project) throw new NotFoundException('Project with id: ${id} not found');
+
+    return await this.projectRepository.remove(project);
+  }
+
+  async createProject( dto: CreateProjectDto ): Promise<projectEntity> {
+
+    const project = this.projectRepository.create({
+      ...dto, 
+      id: randomBytes(16).toString('hex'),
+      user: { /* id: */
+      }
+    });
+
+    await this.projectRepository.save(project);
+
+    return project;
+  }
+}
